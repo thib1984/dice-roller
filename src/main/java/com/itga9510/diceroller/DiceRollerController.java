8f694bd4f1908bcfe380925d641e77327fd8aae4
Thibault Garçon <thibault.garcon@pole-emploi.fr>
2020-07-08 17:59:11 +0200

    FIX limite nb de des nb de faces

--- src/main/java/com/itga9510/diceroller/DiceRollerController.java
+++ src/main/java/com/itga9510/diceroller/DiceRollerController.java
@@ -1,5 +1,8 @@
 package com.itga9510.diceroller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 public class DiceRollerController {
 
     @GetMapping("/{nombresdes}/{facesdes}")
-    public Dices index(@PathVariable int nombresdes, @PathVariable int facesdes) {
+    public ResponseEntity<Object> index(@PathVariable int nombresdes, @PathVariable int facesdes) {
         log.info("appel : /" + nombresdes + "/" + facesdes);
-        return DiceRollerService.diceRoller(nombresdes, facesdes);
+        if (nombresdes <= 0 || nombresdes > 1000 || facesdes <= 0 || facesdes > 1000) {
+            ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "nb de des ou de faces incorrect (1 a 1000 des, 1 a 1000 faces)", "");
+            return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
+        }
+        return new ResponseEntity<Object>(DiceRollerService.diceRoller(nombresdes, facesdes), new HttpHeaders(), HttpStatus.OK);
     }
 
 }
